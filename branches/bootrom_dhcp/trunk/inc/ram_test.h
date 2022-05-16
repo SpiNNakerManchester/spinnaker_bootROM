1ce791924d8840b8c2625bdd3461054fabee5d8b
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2022-05-16 12:03:03 +0100

    Recreate original structure

--- branches/bootrom_dhcp/trunk/inc/ram_test.h
+++
@@ -1,7 +0,0 @@
-#ifndef RAMTEST_H
-	#define RAMTEST_H
-
-	unsigned int ram_test_c (unsigned int startaddr, unsigned int endaddr, unsigned int mask);
-	unsigned int ram_test (unsigned int startaddr, unsigned int endaddr, unsigned int mask);
-	
-#endif
--- tags/bootrom_development_snapshot/trunk/testing/poker.h
+++
@@ -1,8 +0,0 @@
-#ifndef POKER_H
-	#define POKER_H
-
-	unsigned int link_write_word (uint addr, uint link, const uint *buf);
-	unsigned int link_write (uint addr, uint len, uint link, const uint *buf);
-	void poke_neighbours_up(uint len, const uint *buf, uint link);
-	
-#endif
