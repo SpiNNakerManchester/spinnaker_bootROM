7353b574cad64600e9c25c280a325deeb00def95
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2022-05-16 12:05:30 +0100

    Recreate original structure

--- trunk/inc/copier.h
+++
@@ -1,6 +0,0 @@
-#ifndef COPIER_H
-#define COPIER_H
-
-void execute_image(unsigned int src, unsigned int length, unsigned int dest, unsigned int exec_addr);
-
-#endif
--- trunk/inc/reg_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef REG_TEST_H
-#define REG_TEST_H
-
-void test_chip_peripherals(void);
-void test_sysram(void);
-void test_sdram(void); 	// BUGZILLA 56. CP 20/08/2010
-void test_processor_peripherals(void);
-
-#endif
